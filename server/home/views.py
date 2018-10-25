d943b47d59d972b8cb172e72494cd3d73e51286d
Sinclair Liang <sinclairliang@gmail.com>
2018-10-25 16:01:23 -0700

    fixed typo

--- server/home/views.py
+++ server/home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+from django.template import Template, Context
+
+
+# TEMPLATE_DIRS = (
+#     os.path.join(SETTINGS_PATH, 'templates'),
+# )
+
 
 # Create your views here.
 def index(request):
-        return HttpResponse("Home P")
+    t = "server/templates/home.html"
+    return render(request, t)
