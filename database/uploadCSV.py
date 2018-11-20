40057ff5538665d3149da970105c493845095b41
gkchoi <43891129+HybridXSV@users.noreply.github.com>
2018-11-19 20:05:36 -0800

    Fixed error and cleaned up

--- database/uploadCSV.py
+++ database/uploadCSV.py
@@ -1,8 +1,4 @@
-from __future__ import print_function  # Python 2/3 compatibility
 import boto3
-import json
-import decimal
-
 import csv
 
 dynamodb = boto3.resource('dynamodb')
@@ -22,15 +18,12 @@ with open('AP location McHenry 3.csv') as csvDataFile:
             x = row[1]
             y = row[2]
 
-            #readCSV = csv.DictReader(csvfile, fieldNames)
-            # readCSV = csv.reader(csvfile, delimiter=',')
-
             response = table.update_item(
                 Key={
                     'ap_id': ap_id
                 },
                 UpdateExpression="set x = :x, y = :y",
-                # UpdateExpression="set info.rating = :r, info.plot=:p, info.actors=:a",
+
                 ExpressionAttributeValues={
                     ':x': x,
                     ':y': y,
